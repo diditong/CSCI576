7dd1fb24038558fea9d731b6feb6476ed4d00820
diditong <43663301+diditong@users.noreply.github.com>
2021-09-14 04:52:07 -0700

    uploaded hw2

---
+++ hw2_code/Image.h
@@ -0,0 +1,87 @@
+//*****************************************************************************
+//
+// Image.h : Defines the class operations on images
+//
+// Author - Parag Havaldar
+// Main Image class structure 
+//
+//*****************************************************************************
+
+#ifndef IMAGE_DISPLAY
+#define IMAGE_DISPLAY
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include "resource.h"
+#include "afxwin.h"
+
+// C RunTime Header Files
+#include <stdlib.h>
+#include <stdio.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+
+
+typedef struct tagRGB {
+	double r;       // a fraction between 0 and 1
+	double g;       // a fraction between 0 and 1
+	double b;       // a fraction between 0 and 1
+} rgb;
+
+typedef struct tagHSV {
+	double h;       // angle in degrees
+	double s;       // a fraction between 0 and 1
+	double v;       // a fraction between 0 and 1
+} hsv;
+
+// Class structure of Image 
+// Use to encapsulate an RGB image
+class MyImage 
+{
+
+private:
+	int		Width;					// Width of Image
+	int		Height;					// Height of Image
+	char	ImagePath[_MAX_PATH];	// Image location
+	char*	Data;					// RGB data of the image
+
+public:
+	// Constructor
+	MyImage();
+	// Copy Constructor
+	MyImage::MyImage( MyImage *otherImage);
+	// Destructor
+	~MyImage();
+
+	// operator overload
+	MyImage & operator= (const MyImage & otherImage);
+
+	// Reader & Writer functions
+	void	setWidth( const int w)  { Width = w; }; 
+	void	setHeight(const int h) { Height = h; }; 
+	void	setImageData( const char *img ) { Data = (char *)img; };
+	void	setImagePath( const char *path) { strcpy(ImagePath, path); }
+	int		getWidth() { return Width; };
+	int		getHeight() { return Height; };
+	char*	getImageData() { return Data; };
+	char*	getImagePath() { return ImagePath; }
+
+	// Input Output operations
+	bool	ReadImage();
+	bool	WriteImage();
+
+	// Modifications
+	bool	Modify(int, int);
+	double  getCmax(double, double, double);
+	double  getCmin(double, double, double);
+
+
+	hsv rgb2hsv(rgb in);
+	rgb hsv2rgb(hsv in);
+};
+
+#endif //IMAGE_DISPLAY
